4adbf591b8564c3daa10d776355d2d83e97c7e64
Tom Mendy <tom.mendy@epitech.eu>
2023-03-24 15:58:25 +0100

    [CS] remove error

--- lib/my_str/tests/test_load_file_in_memory.c
+++ lib/my_str/tests/test_load_file_in_memory.c
@@ -18,99 +18,99 @@ Test(load_file_in_memory, basic) {
 ## makefile\n\
 ##\n\
 \n\
-SRC     =	my_count_nb_char_in_str.c					\\\n\
-			my_is_alpha.c								\\\n\
-			my_is_lower.c								\\\n\
-			my_is_num.c									\\\n\
-			my_is_printable.c							\\\n\
-			my_is_upper.c								\\\n\
-			my_load_file_in_array.c						\\\n\
-			my_load_file_in_memory.c					\\\n\
-			my_put_char.c								\\\n\
-			my_put_str.c								\\\n\
-			my_rev_str.c								\\\n\
-			my_show_word_array.c						\\\n\
-			my_str_capitalize.c							\\\n\
-			my_str_cat.c								\\\n\
-			my_str_cmp.c								\\\n\
-			my_str_cpy.c								\\\n\
-			my_str_is_alpha.c							\\\n\
-			my_str_is_lower.c							\\\n\
-			my_str_is_num.c								\\\n\
-			my_str_is_printable.c						\\\n\
-			my_str_is_upper.c							\\\n\
-			my_str_len.c								\\\n\
-			my_str_low_case.c							\\\n\
-			my_str_n_cat.c								\\\n\
-			my_str_n_cmp.c								\\\n\
-			my_str_n_cpy.c								\\\n\
-			my_str_str.c								\\\n\
-			my_str_up_case.c							\\\n\
-			spliter/add_word.c							\\\n\
-			spliter/change_begin_str.c					\\\n\
-			spliter/generate_tab.c						\\\n\
-			spliter/str_only_contain_str_delimiter.c	\\\n\
-			spliter/spliter.c							\\\n\
-			my_swap.c									\\\n\
+SRC     =\tmy_count_nb_char_in_str.c\t\t\t\t\t\\\n\
+\t\t\tmy_is_alpha.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_is_lower.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_is_num.c\t\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_is_printable.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_is_upper.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_load_file_in_array.c\t\t\t\t\t\t\\\n\
+\t\t\tmy_load_file_in_memory.c\t\t\t\t\t\\\n\
+\t\t\tmy_put_char.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_put_str.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_rev_str.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_show_word_array.c\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_capitalize.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_cat.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_cmp.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_cpy.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_is_alpha.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_is_lower.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_is_num.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_is_printable.c\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_is_upper.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_len.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_low_case.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_n_cat.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_n_cmp.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_n_cpy.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_str.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_str_up_case.c\t\t\t\t\t\t\t\\\n\
+\t\t\tspliter/add_word.c\t\t\t\t\t\t\t\\\n\
+\t\t\tspliter/change_begin_str.c\t\t\t\t\t\\\n\
+\t\t\tspliter/generate_tab.c\t\t\t\t\t\t\\\n\
+\t\t\tspliter/str_only_contain_str_delimiter.c\t\\\n\
+\t\t\tspliter/spliter.c\t\t\t\t\t\t\t\\\n\
+\t\t\tmy_swap.c\t\t\t\t\t\t\t\t\t\\\n\
 \n\
-DIR_SRC	=		$(addprefix src/, $(SRC))\n\
+DIR_SRC\t=\t\t$(addprefix src/, $(SRC))\n\
 \n\
-TEST	=	test_my_put_char.c							\\\n\
-			test_load_file_in_memory.c					\\\n\
-			test_my_put_str.c							\\\n\
-			test_my_rev_str.c							\\\n\
-			test_my_show_word_array.c					\\\n\
-			test_my_str_capitalize.c					\\\n\
-			test_my_str_cat.c							\\\n\
-			test_my_str_cmp.c							\\\n\
-			test_my_str_cpy.c							\\\n\
-			test_my_str_is_alpha.c						\\\n\
-			test_my_str_is_lower.c						\\\n\
-			test_my_str_is_num.c						\\\n\
-			test_my_str_is_printable.c					\\\n\
-			test_my_str_is_upper.c						\\\n\
-			test_my_str_len.c							\\\n\
-			test_my_str_low_case.c						\\\n\
-			test_my_str_n_cat.c							\\\n\
-			test_my_str_n_cmp.c							\\\n\
-			test_my_str_n_cpy.c							\\\n\
-			test_my_str_str.c							\\\n\
-			test_my_str_up_case.c						\\\n\
-			test_my_swap.c								\\\n\
-			test_spliter.c								\\\n\
+TEST\t=\ttest_my_put_char.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_load_file_in_memory.c\t\t\t\t\t\\\n\
+\t\t\ttest_my_put_str.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_rev_str.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_show_word_array.c\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_capitalize.c\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_cat.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_cmp.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_cpy.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_is_alpha.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_is_lower.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_is_num.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_is_printable.c\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_is_upper.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_len.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_low_case.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_n_cat.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_n_cmp.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_n_cpy.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_str.c\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_str_up_case.c\t\t\t\t\t\t\\\n\
+\t\t\ttest_my_swap.c\t\t\t\t\t\t\t\t\\\n\
+\t\t\ttest_spliter.c\t\t\t\t\t\t\t\t\\\n\
 \n\
-DIR_TESTS	=		$(addprefix tests/, $(TEST))\n\
+DIR_TESTS\t=\t\t$(addprefix tests/, $(TEST))\n\
 \n\
-OBJ			=		$(DIR_SRC:.c=.o)\n\
+OBJ\t\t\t=\t\t$(DIR_SRC:.c=.o)\n\
 \n\
-OBJ_TESTS	=		$(DIR_TESTS:.c=.o)\n\
+OBJ_TESTS\t=\t\t$(DIR_TESTS:.c=.o)\n\
 \n\
-CPPFLAGS	= -I include -Wextra -Wall -ggdb3\n\
+CPPFLAGS\t= -I include -Wextra -Wall -ggdb3\n\
 \n\
-NAME		=		../libmy_str.a\n\
+NAME\t\t=\t\t../libmy_str.a\n\
 \n\
-$(NAME):		$(OBJ)\n\
-	ar rcs $(NAME) $(OBJ)\n\
+$(NAME):\t\t$(OBJ)\n\
+\tar rcs $(NAME) $(OBJ)\n\
 \n\
 all: $(NAME)\n\
 \n\
 clean:\n\
-	rm -f $(OBJ)\n\
+\trm -f $(OBJ)\n\
 \n\
 fclean: clean\n\
-	rm -rf unit_tests\n\
-	rm -rf *.gcov\n\
-	rm -f $(NAME)\n\
+\trm -rf unit_tests\n\
+\trm -rf *.gcov\n\
+\trm -f $(NAME)\n\
 \n\
 re: fclean all\n\
 \n\
 unit_tests: re\n\
-	mkdir unit_tests\n\
-		gcc -o unit_tests/unit_tests $(DIR_SRC) $(DIR_TESTS) -I include/\\\n\
-		--coverage -lcriterion\n\
+\tmkdir unit_tests\n\
+\t\tgcc -o unit_tests/unit_tests $(DIR_SRC) $(DIR_TESTS) -I include/\\\n\
+\t\t--coverage -lcriterion\n\
 \n\
-tests_run:	unit_tests\n\
-			./unit_tests/unit_tests\n\
+tests_run:\tunit_tests\n\
+\t\t\t./unit_tests/unit_tests\n\
 \n\
 .PHONY: all clean fclean re tests_run\n\
 ";
